69004b1e2a0e163520ec55a980da02ff385fccfe
Aleksej Morozko <aleksejmorozko@gmail.com>
2020-04-10 14:59:18 +0300

    fetch2

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,19 +1,35 @@
-const requestURL = 'https://jsonplaceholder.typicode.com/users';
+const requestURL = 'https://jsonplaceholder.typicode.com/usersd';
 
-function sendRequest(method, url, body){
-    return fetch(url)    
+function sendRequest(method, url, body=null){
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        if (response.ok){
+            return response.json()
+        }
+        return response.json().then(error => {
+            const e = new Error('Somthing wrong');
+            e.data = error;
+            throw e
+        })
+    })    
 }
 
- sendRequest('GET', requestURL)
+/*  sendRequest('GET', requestURL)
     .then(data => console.log(data))
     .catch(err => console.log(err))
- 
+  */
 
 const body = {
     name: 'alex',
     age: 34
 }
 
-/* sendRequest('POST', requestURL, body)
+sendRequest('POST', requestURL, body)
     .then(data => console.log(data))
-    .catch(err => console.log(err)) */
+    .catch(err => console.log(err)) 
