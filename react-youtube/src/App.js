b27c1198c34920854d44ad01736b45a61bb439aa
Aleksej Morozko <aleksejmorozko@gmail.com>
2020-04-14 16:31:39 +0300

    react_yotube_ready

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TodoList from './Todo/TodoList';
+import Context from './context';
+import AddTodo from './Todo/AddTodo';
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -13,17 +15,32 @@ function App() {
       todos.map(todo => {
         if (todo.id === id) {
             todo.completed = !todo.completed
-        }
+        } 
         return todo
       })
     )   
   } 
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title){
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
 
   return (
-    <div className='wrapper'>
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
         <h1>React Tutorial</h1>
-        <TodoList todos={todos} onToggle={toggleTodo}/>
-    </div>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ?  <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos</p>}
+       
+      </div>
+    </Context.Provider>    
   );
 } 
 export default App;
--- react-youtube/src/Todo/TodoList.js
+++ react-youtube/src/Todo/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
+
+
 const styles = {
     ul: {
         listStyle: 'none',
@@ -9,7 +11,7 @@ const styles = {
     }
 }
 
-function TodoList(props) {
+function TodoList(props) {    
     return (
         <ul style={styles.ul}>
             {props.todos.map((todo, index) => {
